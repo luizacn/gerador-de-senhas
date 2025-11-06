547a97369044973c2f895306f4f4247328dbf862
Luiz abreu <luizcna2022@gmail.com>
2025-11-05 22:09:01 -0300

    iniciando a interatividade com javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    let pass = "b!h";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+
+    }
+}
