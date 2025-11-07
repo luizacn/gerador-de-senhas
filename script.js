af4c72b67bc867bda6003c238057dc59747846b6
Luiz abreu <luizcna2022@gmail.com>
2025-11-06 21:49:27 -0300

    terminando interação do gerador de senhas

--- script.js
+++ script.js
@@ -6,18 +6,31 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function(){
+slider.oninput = function () {
     sizePassword.innerHTML = this.value;
 }
 
-function generatePassword(){
-    let pass = "b!h";
-    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-
+function generatePassword() {
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
     }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
 }
+
+function copyPassword() {
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.whiteText(novaSenha);
+}
+
+
+
+
